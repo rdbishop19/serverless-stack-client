51dff8d6b6af7ad6ecb963032c71a11d3812598f
Ryan Bishop <rdbishop06@gmail.com>
2020-10-23 00:04:45 -0500

    add Amplify auth to app

--- src/App.js
+++ src/App.js
@@ -1,35 +1,49 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Nav, Navbar, NavItem } from "react-bootstrap";
-import { LinkContainer } from "react-router-bootstrap";
-import "./App.css";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Nav, Navbar, NavItem } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import './App.css';
 
-import Routes from "./Routes";
+import Routes from './Routes';
+import { AppContext } from './libs/contextLibs';
 
 function App() {
-  return (
-    <div className="App container">
-      <Navbar fluid collapseOnSelect>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <Link to="/">Scratch</Link>
-          </Navbar.Brand>
-          <Navbar.Toggle />
-        </Navbar.Header>
-        <Navbar.Collapse>
-          <Nav pullRight>
-            <LinkContainer to="/signup">
-              <NavItem>Signup</NavItem>
-            </LinkContainer>
-            <LinkContainer to="/login">
-              <NavItem>Login</NavItem>
-            </LinkContainer>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-      <Routes />
-    </div>
-  );
+  const [ isAuthenticated, userHasAuthenticated ] = useState(false);
+  
+  const handleLogout = () => {
+    userHasAuthenticated(false);
+  }
+
+	return (
+		<div className="App container">
+			<Navbar fluid collapseOnSelect>
+				<Navbar.Header>
+					<Navbar.Brand>
+						<Link to="/">Scratch</Link>
+					</Navbar.Brand>
+					<Navbar.Toggle />
+				</Navbar.Header>
+				<Navbar.Collapse>
+					<Nav pullRight>
+          {isAuthenticated 
+            ? <NavItem onClick={handleLogout}>Logout</NavItem>
+            : <React.Fragment>
+                <LinkContainer to="/signup">
+                  <NavItem>Signup</NavItem>
+                </LinkContainer>
+                <LinkContainer to="/login">
+                  <NavItem>Login</NavItem>
+                </LinkContainer>
+              </React.Fragment>
+          }
+					</Nav>
+				</Navbar.Collapse>
+			</Navbar>
+			<AppContext.Provider value={{ isAuthenticated, userHasAuthenticated }}>
+				<Routes />
+			</AppContext.Provider>
+		</div>
+	);
 }
 
 export default App;
