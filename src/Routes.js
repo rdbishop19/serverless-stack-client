26c01c817a1e7af4265ce77882b2aa24fe3b65cb
Ryan Bishop <rdbishop06@gmail.com>
2020-10-25 21:11:05 -0500

    signup process + create note working

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './containers/Home';
 import Login from './containers/Login';
+import NewNote from './containers/NewNote';
 import NotFound from './containers/NotFound';
 import Signup from './containers/Signup';
 
@@ -16,6 +17,9 @@ export default function Routes() {
 			</Route>
 			<Route exact path="/signup">
 				<Signup />
+			</Route>
+			<Route exact path="/notes/new">
+				<NewNote />
 			</Route>
 			{/* Finally, catch all unmatched routes */}
 			<Route>
