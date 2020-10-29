751b59cbe97daf0d812346ab7147bda5b8a2e083
Ryan Bishop <rdbishop06@gmail.com>
2020-10-28 23:48:22 -0500

    Adding Netlify build script

--- src/config.js
+++ src/config.js
@@ -1,18 +1,46 @@
+const dev = {
+	STRIPE_KEY:
+		'pk_test_51HfE0HJ4jumlVfD4Le5RT6gQkc7p7Lb6L0lFbe1or4167R3rgk5meXI7KabNHZk8znf3EEZ63rm3CnbuoApRlHx500IUJOhe5F',
+	s3: {
+		REGION: 'us-east-2',
+		BUCKET: 'rdb-serverless-notes-upload'
+	},
+	apiGateway: {
+		REGION: 'us-east-2',
+		URL: 'https://api.devtakes.com/dev'
+	},
+	cognito: {
+		REGION: 'us-east-2',
+		USER_POOL_ID: 'us-east-2_CzJkcMkio',
+		APP_CLIENT_ID: '52fsuvq5jg5n05tis5nr7nk80a',
+		IDENTITY_POOL_ID: 'us-east-2:3e612fe9-05ab-49a0-b7e9-3e199bc4edf6'
+	}
+};
+
+const prod = {
+	STRIPE_KEY:
+		'pk_test_51HfE0HJ4jumlVfD4Le5RT6gQkc7p7Lb6L0lFbe1or4167R3rgk5meXI7KabNHZk8znf3EEZ63rm3CnbuoApRlHx500IUJOhe5F',
+	s3: {
+		REGION: 'us-east-2',
+		BUCKET: 'rdb-serverless-notes-upload'
+	},
+	apiGateway: {
+		REGION: 'us-east-2',
+		URL: 'https://api.devtakes.com/prod'
+	},
+	cognito: {
+		REGION: 'us-east-2',
+		USER_POOL_ID: 'us-east-2_ZiPMrt3mY',
+		APP_CLIENT_ID: '5rjq00kfclih2ec9fnh03vdm7',
+		IDENTITY_POOL_ID: 'us-east-2:5c9eae6b-a59f-4e14-a903-0f78a750a481'
+	}
+};
+
+// Default to dev if not set
+const config = process.env.REACT_APP_STAGE === 'prod' ? prod : dev;
+
 export default {
-    MAX_ATTACHMENT_SIZE: 5000000,
-    STRIPE_KEY: "pk_test_51HfE0HJ4jumlVfD4Le5RT6gQkc7p7Lb6L0lFbe1or4167R3rgk5meXI7KabNHZk8znf3EEZ63rm3CnbuoApRlHx500IUJOhe5F",
-    s3: {
-      REGION: "us-east-2",
-      BUCKET: "rdb-serverless-notes-upload"
-    },
-    apiGateway: {
-      REGION: "us-east-2",
-      URL: "https://92hek2kbf6.execute-api.us-east-2.amazonaws.com/prod"
-    },
-    cognito: {
-      REGION: "us-east-2",
-      USER_POOL_ID: "us-east-2_w2vQBGEjD",
-      APP_CLIENT_ID: "2o9tg48f37p5j9kvdfcsodpj9",
-      IDENTITY_POOL_ID: "us-east-2:b9325c28-0cd1-48bf-b6f9-1f29fe8028f8"
-    }
-  };
+	// Add common config values here
+	MAX_ATTACHMENT_SIZE: 5000000,
+	...config
+};
