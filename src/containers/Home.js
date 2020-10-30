6485bed0250e325bef484521d668002723549a23
Ryan Bishop <rdbishop06@gmail.com>
2020-10-29 22:15:05 -0500

    Updating the lander

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,6 +2,7 @@ import { API } from "aws-amplify";
 import React, { useState, useEffect } from "react";
 import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
+import { Link } from "react-router-dom";
 import { useAppContext } from "../libs/contextLib";
 import { onError } from "../libs/errorLib";
 import "./Home.css";
@@ -59,10 +60,18 @@ export default function Home() {
       <div className="lander">
         <h1>Scratch</h1>
         <p>A simple note taking app</p>
+        <div>
+          <Link to="/login" className="btn btn-info btn-lg">
+            Login
+          </Link>
+          <Link to="/signup" className="btn btn-success btn-lg">
+            Signup
+          </Link>
+        </div>
       </div>
     );
   }
-
+  
   function renderNotes() {
     return (
       <div className="notes">
